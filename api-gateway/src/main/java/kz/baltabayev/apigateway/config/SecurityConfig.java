d4d3a594fab9c4c7d08a667744b7ffed97101d82
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-04 13:28:18 +0600

    initialization of the discovery module and implementation of servers and clients

---
+++ api-gateway/src/main/java/kz/baltabayev/apigateway/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package kz.baltabayev.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/eureka/**").permitAll()
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(spec -> spec.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
---
+++ discovery-service/src/test/java/kz/baltabayev/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.baltabayev.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
