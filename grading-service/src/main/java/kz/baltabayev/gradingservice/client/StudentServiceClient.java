c73c50871e6c675906cf78a13b4223e997b6bc52
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-08 05:45:52 +0600

    changed StudentServiceClient -> deleted url and added path

--- grading-service/src/main/java/kz/baltabayev/gradingservice/client/StudentServiceClient.java
+++ grading-service/src/main/java/kz/baltabayev/gradingservice/client/StudentServiceClient.java
@@ -3,7 +3,7 @@ package kz.baltabayev.gradingservice.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "student-service", url = "http//localhost:8080/students")
+@FeignClient(name = "student-service", path = "/students")
 public interface StudentServiceClient {
 
     //todo: logic here
--- identity-service/src/main/java/kz/baltabayev/identityservice/client/StudentServiceClient.java
+++ identity-service/src/main/java/kz/baltabayev/identityservice/client/StudentServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "student-service", url = "http//localhost:8080/students")
+@FeignClient(name = "student-service", path = "/students")
 public interface StudentServiceClient {
 
     @PostMapping("/create")
