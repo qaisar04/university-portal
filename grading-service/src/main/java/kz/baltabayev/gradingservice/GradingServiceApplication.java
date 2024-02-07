0384edb4c43f9241fdb1d04ee969eba49bc20e95
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-07 10:02:07 +0600

    init grading-service

---
+++ grading-service/src/main/java/kz/baltabayev/gradingservice/GradingServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.baltabayev.gradingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradingServiceApplication.class, args);
+	}
+
+}
---
+++ grading-service/src/test/java/kz/baltabayev/gradingservice/GradingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.baltabayev.gradingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
