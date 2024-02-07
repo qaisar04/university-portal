3aea2b66b20b43ba8394f09cf04a25f10831877b
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-07 19:00:27 +0600

    fixed a bug in RedisCacheConfig

---
+++ invitation-code-service/src/main/java/kz/baltabayev/invitationcodeservice/controller/InviteCodeController.java
@@ -0,0 +1,33 @@
+package kz.baltabayev.invitationcodeservice.controller;
+
+import kz.baltabayev.invitationcodeservice.exception.InvalidRoleException;
+import kz.baltabayev.invitationcodeservice.model.types.Role;
+import kz.baltabayev.invitationcodeservice.service.InviteCodeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/invite-code")
+@RequiredArgsConstructor
+public class InviteCodeController {
+
+    public final InviteCodeService inviteCodeService;
+
+    @GetMapping("/{role}")
+    public ResponseEntity<String> generate(@PathVariable String role) {
+        try {
+            return ResponseEntity.ok(inviteCodeService.generateInviteCode(Role.valueOf(role.toUpperCase())));
+        } catch (IllegalArgumentException e) {
+            throw new InvalidRoleException("Invalid role: " + role);
+        }
+    }
+
+    @GetMapping("/role/{code}")
+    public ResponseEntity<Role> getRole(@PathVariable String code) {
+        return ResponseEntity.ok(inviteCodeService.getRoleByInviteCode(code));
+    }
+}
---
+++ invitation-code-service/src/main/java/kz/baltabayev/invitationcodeservice/exception/InvalidRoleException.java
@@ -0,0 +1,7 @@
+package kz.baltabayev.invitationcodeservice.exception;
+
+public class InvalidRoleException extends IllegalArgumentException {
+    public InvalidRoleException(String s) {
+        super(s);
+    }
+}
