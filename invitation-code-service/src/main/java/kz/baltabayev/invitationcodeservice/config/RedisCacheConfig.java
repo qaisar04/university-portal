0553e05c1aa1524e11f3b7c3a4a81820ce06080a
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-07 18:45:07 +0600

    fixed a bug in RedisCacheConfig

--- invitation-code-service/src/main/java/kz/baltabayev/invitationcodeservice/config/RedisCacheConfig.java
+++ invitation-code-service/src/main/java/kz/baltabayev/invitationcodeservice/config/RedisCacheConfig.java
@@ -1,19 +1,20 @@
 package kz.baltabayev.invitationcodeservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
 
-import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.*;
+import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer;
 
 @Configuration
 public class RedisCacheConfig {
 
     private static final int CACHE_TTL_MINUTES = 60;
 
+    @Bean
     public RedisCacheConfiguration cacheConfiguration() {
         return RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(CACHE_TTL_MINUTES))
