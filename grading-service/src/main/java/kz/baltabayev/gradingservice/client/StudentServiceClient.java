adc2b953807fa8fae7493b3a521374216cea9968
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-08 10:57:43 +0600

    added method for StudentServiceClient

--- grading-service/src/main/java/kz/baltabayev/gradingservice/client/StudentServiceClient.java
+++ grading-service/src/main/java/kz/baltabayev/gradingservice/client/StudentServiceClient.java
@@ -1,10 +1,16 @@
 package kz.baltabayev.gradingservice.client;
 
+import kz.baltabayev.gradingservice.model.entity.Student;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @FeignClient(name = "student-service", path = "/students")
 public interface StudentServiceClient {
 
-    //todo: logic here
+    @GetMapping
+    ResponseEntity<List<Student>> getAllStudents();
+
 }
