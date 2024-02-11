2aa53d63314287d569ca4503439d1d19635805c7
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-11 17:08:12 +0600

    new endpoint

--- grading-service/src/main/java/kz/baltabayev/gradingservice/controller/GradingController.java
+++ grading-service/src/main/java/kz/baltabayev/gradingservice/controller/GradingController.java
@@ -1,7 +1,41 @@
 package kz.baltabayev.gradingservice.controller;
 
+import kz.baltabayev.gradingservice.mapper.GradeMapper;
+import kz.baltabayev.gradingservice.model.dto.GradeDto;
+import kz.baltabayev.gradingservice.model.entity.Grade;
+import kz.baltabayev.gradingservice.service.GradeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RestController
+@RequestMapping("/grade")
+@RequiredArgsConstructor
 public class GradingController {
+
+    private final GradeService gradeService;
+    private final GradeMapper gradeMapper;
+
+    @PostMapping("/add")
+    public ResponseEntity<GradeDto> addGrade(@RequestBody GradeDto gradeDto) {
+        gradeService.save(gradeMapper.toEntity(gradeDto));
+        return ResponseEntity.ok(gradeDto);
+    }
+
+    @GetMapping("/average/{studentId}")
+    public ResponseEntity<Double> getAverageScore(@PathVariable Long studentId) {
+        Double avgScore = gradeService.getAverageScoreByStudentId(studentId);
+        return ResponseEntity.ok(avgScore);
+    }
+
+
+
+
+
+
+
+
+
 }
