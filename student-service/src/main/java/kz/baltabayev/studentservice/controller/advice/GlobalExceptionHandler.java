b1c80b9771e658f8577d949e7c2a33032e558aa0
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-11 21:54:22 +0600

    added global exception handler

---
+++ student-service/src/main/java/kz/baltabayev/studentservice/controller/advice/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package kz.baltabayev.studentservice.controller.advice;
+
+import kz.baltabayev.studentservice.exception.StudentNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(StudentNotFoundException.class)
+    ProblemDetail handlerStudentNotFoundException(StudentNotFoundException exception) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, exception.getMessage());
+    }
+}
