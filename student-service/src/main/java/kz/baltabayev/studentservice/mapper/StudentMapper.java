d5ec50860e9650d542034c12922f53ee70f60cb3
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-15 23:30:58 +0600

    added mapping for better read

--- student-service/src/main/java/kz/baltabayev/studentservice/mapper/StudentMapper.java
+++ student-service/src/main/java/kz/baltabayev/studentservice/mapper/StudentMapper.java
@@ -2,14 +2,26 @@ package kz.baltabayev.studentservice.mapper;
 
 import kz.baltabayev.studentservice.model.dto.StudentRequest;
 import kz.baltabayev.studentservice.model.entity.Student;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface StudentMapper {
 
-    @Mapping(target = "id", ignore = true)
+    @Mappings({
+            @Mapping(target = "id", ignore = true),
+            @Mapping(target = "email", source = "email"),
+            @Mapping(target = "studentInfo.firstname", source = "name"),
+            @Mapping(target = "studentInfo.lastname", source = "lasname"),
+            @Mapping(target = "studentInfo.birthDate", source = "birthdate"),
+            @Mapping(target = "studentInfo.gender", source = "gender"),
+            @Mapping(target = "studentInfo.course", source = "course"),
+            @Mapping(target = "studentInfo.facultyId", source = "facultyId")
+    })
     Student toStudent(StudentRequest studentRequest);
 
-    StudentRequest toDTO(Student student);
+    @InheritInverseConfiguration(name = "toStudent")
+    StudentRequest toDto(Student student);
 }
