107f90a876ca3508bddca13e8d40ad542c280f51
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-15 23:29:28 +0600

    logic for save to db

--- student-service/src/main/java/kz/baltabayev/studentservice/comsumer/StudentConsumer.java
+++ student-service/src/main/java/kz/baltabayev/studentservice/comsumer/StudentConsumer.java
@@ -1,21 +1,28 @@
 package kz.baltabayev.studentservice.comsumer;
 
+import kz.baltabayev.studentservice.model.dto.StudentRequest;
 import kz.baltabayev.studentservice.model.entity.Student;
+import kz.baltabayev.studentservice.model.entity.StudentInfo;
 import kz.baltabayev.studentservice.model.service.impl.StudentServiceImpl;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Component
-@Slf4j
 @RequiredArgsConstructor
 public class StudentConsumer {
 
     private final StudentServiceImpl studentService;
 
     @KafkaListener(topics = "${spring.kafka.queues.student}", groupId = "student-service")
-    public void consumeStudent(Student student) {
-        studentService.save(student);
+    public void consumeStudent(StudentRequest studentRequest) {
+        studentService.save(Student.builder()
+                .email(studentRequest.email())
+                .studentInfo(StudentInfo.builder()
+                        .firstname(studentRequest.name())
+                        .build())
+                .build());
     }
 }
--- student-service/src/main/java/kz/baltabayev/studentservice/model/dto/StudentInfoRequest.java
+++ student-service/src/main/java/kz/baltabayev/studentservice/model/dto/StudentInfoRequest.java
@@ -1,17 +1,16 @@
 package kz.baltabayev.studentservice.model.dto;
 
-import kz.baltabayev.studentservice.model.enums.FacultyName;
 import kz.baltabayev.studentservice.model.enums.Gender;
 
 import java.time.LocalDate;
 
-public record StudentRequest(
+public record StudentInfoRequest(
         String name,
         String lasname,
         LocalDate birthdate,
         String email,
         Gender gender,
         Integer course,
-        FacultyName faculty
+        Long facultyId
 ) {
 }
