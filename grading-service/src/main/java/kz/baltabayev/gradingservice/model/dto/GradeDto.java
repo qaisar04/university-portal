81e63f842fc73a243f7c351b582eab1de5b6240c
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-16 22:12:19 +0600

    new dto

---
+++ grading-service/src/main/java/kz/baltabayev/gradingservice/model/dto/GradeDto.java
@@ -0,0 +1,8 @@
+package kz.baltabayev.gradingservice.model.dto;
+
+/**
+ * Data Transfer Object for the Grade entity.
+ * This record encapsulates the data that can be transferred between processes or across the network.
+ */
+public record GradeDto(double score, long studentId, long subjectId) {
+}
