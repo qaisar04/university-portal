e8b95752908919fe7e1faad6e79819a030667b98
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-16 22:13:12 +0600

    javadoc

--- grading-service/src/main/java/kz/baltabayev/gradingservice/controller/GradingController.java
+++ grading-service/src/main/java/kz/baltabayev/gradingservice/controller/GradingController.java
@@ -2,6 +2,7 @@ package kz.baltabayev.gradingservice.controller;
 
 import kz.baltabayev.gradingservice.mapper.GradeMapper;
 import kz.baltabayev.gradingservice.model.dto.GradeDto;
+import kz.baltabayev.gradingservice.model.dto.GradeResponse;
 import kz.baltabayev.gradingservice.model.entity.Grade;
 import kz.baltabayev.gradingservice.service.GradeService;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +12,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
+/**
+ * Controller for the Grade entity.
+ * This controller provides endpoints for performing CRUD operations on grades.
+ */
 @Controller
 @RestController
 @RequestMapping("/api/v1/grade")
@@ -22,36 +26,70 @@ public class GradingController {
     private final GradeService gradeService;
     private final GradeMapper gradeMapper;
 
+    /**
+     * Controller for the Grade entity.
+     * This controller provides endpoints for performing CRUD operations on grades.
+     */
     @PostMapping("/add")
-    public ResponseEntity<GradeDto> addGrade(@RequestBody GradeDto gradeDto) {
-        gradeService.save(gradeMapper.toEntity(gradeDto));
-        return ResponseEntity.ok(gradeDto);
+    public ResponseEntity<GradeDto> addGrade(@RequestBody GradeDto request) {
+        GradeDto saved = gradeService.save(gradeMapper.toEntity(request));
+        return ResponseEntity.ok(saved);
     }
 
+    /**
+     * Retrieves the average score for a specific student.
+     *
+     * @param studentId the ID of the student
+     * @return the average score
+     */
     @GetMapping("/average/{studentId}")
     public ResponseEntity<Double> getAverageScore(@PathVariable Long studentId) {
         Double avgScore = gradeService.getAverageScoreByStudentId(studentId);
         return ResponseEntity.ok(avgScore);
     }
 
+    /**
+     * Retrieves all grades for a specific student, grouped by subject.
+     *
+     * @param studentId the ID of the student
+     * @return a map where the key is the subject ID and the value is a list of grades for that subject
+     */
     @GetMapping("/student/{studentId}")
-    public ResponseEntity<Map<Long, List<GradeDto>>> getByStudentId(@PathVariable Long studentId) {
-        Map<Long, List<GradeDto>> gradingList = gradeService.getByStudentId(studentId);
+    public ResponseEntity<Map<Long, List<GradeResponse>>> getByStudentId(@PathVariable Long studentId) {
+        Map<Long, List<GradeResponse>> gradingList = gradeService.getByStudentId(studentId);
         return ResponseEntity.ok(gradingList);
     }
 
+    /**
+     * Updates a grade.
+     *
+     * @param request the grade to update
+     * @return the updated grade
+     */
     @PutMapping("/update")
-    public ResponseEntity<?> updateGrade(@RequestBody GradeDto gradeDto) {
-        Grade updatedDrade = gradeService.update(gradeMapper.toEntity(gradeDto));
+    public ResponseEntity<?> updateGrade(@RequestBody GradeDto request) {
+        Grade updatedDrade = gradeService.update(gradeMapper.toEntity(request));
         return ResponseEntity.ok(updatedDrade);
     }
 
+    /**
+     * Deletes a grade by its ID.
+     *
+     * @param id the ID of the grade
+     * @return a message indicating that the grade was successfully deleted
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteGrade(@PathVariable Long id) {
         gradeService.deleteById(id);
         return ResponseEntity.ok("Grade with id %s was successfully deleted".formatted(id));
     }
 
+    /**
+     * Deletes all grades for a specific student.
+     *
+     * @param studentId the ID of the student
+     * @return a message indicating that the grades were successfully deleted
+     */
     @DeleteMapping("/student/{studentId}")
     public ResponseEntity<String> deleteGradeByStudentId(@PathVariable Long studentId) {
         gradeService.deleteByStudentId(studentId);
