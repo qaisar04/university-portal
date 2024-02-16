5503999fbb320af3ebaae00334b4278ec9acb35d
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-16 22:09:30 +0600

    added javadoc

--- grading-service/src/main/java/kz/baltabayev/gradingservice/exception/GradeNotFoundException.java
+++ grading-service/src/main/java/kz/baltabayev/gradingservice/exception/GradeNotFoundException.java
@@ -1,6 +1,16 @@
 package kz.baltabayev.gradingservice.exception;
 
+/**
+ * Exception thrown when a grade is not found.
+ * This exception is used to indicate that a grade with a specific ID does not exist.
+ */
 public class GradeNotFoundException extends RuntimeException {
+
+    /**
+     * Constructs a new GradeNotFoundException with a detail message.
+     *
+     * @param id the ID of the grade that was not found
+     */
     public GradeNotFoundException(Long id) {
         super("Grade with id %s not found".formatted(id));
     }
