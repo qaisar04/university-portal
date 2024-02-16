ad8a3e601c2084e79b4be65bd083cccbe6ddac7f
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-16 23:32:52 +0600

    init sheduling-service

---
+++ scheduling-service/src/main/java/kz/baltabayev/shedulingservice/ShedulingServiceApplication.java
@@ -0,0 +1,11 @@
+package kz.baltabayev.shedulingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShedulingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShedulingServiceApplication.class, args);
+    }
+}
---
+++ subject-service/src/main/java/kz/baltabayev/subjectservice/controller/SubjectController.java
@@ -0,0 +1,23 @@
+package kz.baltabayev.subjectservice.controller;
+
+import kz.baltabayev.subjectservice.model.entity.Subject;
+import kz.baltabayev.subjectservice.service.SubjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/subject")
+@RequiredArgsConstructor
+public class SubjectController {
+
+    private final SubjectService subjectService;
+
+    @PostMapping("/add")
+    public ResponseEntity<Subject> create(@RequestBody Subject subject) {
+        return ResponseEntity.ok(subjectService.save(subject));
+    }
+}
