a5e275c1ebb25ad4afda5ea33aeb0a89c535862f
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-16 21:24:33 +0600

    delete client

--- grading-service/src/main/java/kz/baltabayev/gradingservice/GradingServiceApplication.java
+++ grading-service/src/main/java/kz/baltabayev/gradingservice/GradingServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class GradingServiceApplication {
 
 	public static void main(String[] args) {
