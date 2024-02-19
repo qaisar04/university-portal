6b70f56d0ffb40011c349bb27fe7775f19210771
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-19 13:31:38 +0600

    added new client controller and refactoring an existing one

--- student-service/src/main/java/kz/baltabayev/studentservice/client/GradingServiceClient.java
+++ student-service/src/main/java/kz/baltabayev/studentservice/client/GradingServiceClient.java
@@ -8,14 +8,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Map;
 
 
 @FeignClient(name = "grading-service", path = "/api/v1/grade")
 public interface GradingServiceClient {
 
     @GetMapping("/student/{studentId}")
-    ResponseEntity<List<GradeResponse>> getByStudentId(@PathVariable Long studentId);
+    ResponseEntity<Map<Long, List<GradeResponse>>> getByStudentId(@PathVariable Long studentId);
 
     @DeleteMapping("/student/{studentId}")
     ResponseEntity<String> deleteByStudentId(@PathVariable Long studentId);
+
+    @GetMapping("/average/{studentId}")
+    ResponseEntity<Double> getAverageScore(@PathVariable Long studentId);
 }
