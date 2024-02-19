32ce5b5060743ebbe7532f02ba6379912914979f
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-02-19 13:34:17 +0600

    base entities

--- scheduling-service/src/main/java/kz/baltabayev/shedulingservice/model/entity/Lesson.java
+++ scheduling-service/src/main/java/kz/baltabayev/shedulingservice/model/entity/Lesson.java
@@ -1,8 +1,22 @@
 package kz.baltabayev.shedulingservice.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Lesson {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long teacherId;
     private Long subjectId;
--- scheduling-service/src/main/java/kz/baltabayev/shedulingservice/model/entity/Schedule.java
+++ scheduling-service/src/main/java/kz/baltabayev/shedulingservice/model/entity/Schedule.java
@@ -1,10 +1,27 @@
 package kz.baltabayev.shedulingservice.model.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Schedule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long groupId;
     private Long facultyId;
+    @ManyToMany
+    @JoinTable(
+            name = "schedule_lesson",
+            joinColumns = @JoinColumn(name = "schedule_id"),
+            inverseJoinColumns = @JoinColumn(name = "lesson_id")
+    )
     private List<Lesson> lessons;
 }
