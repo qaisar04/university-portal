61a35eff3faedd1eb5488066b441f8fbc9b0bdba
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-03-02 14:04:50 +0500

    resolve problem serialize multipartfile

--- student-service/src/main/java/kz/baltabayev/studentservice/client/StorageServiceClient.java
+++ student-service/src/main/java/kz/baltabayev/studentservice/client/StorageServiceClient.java
@@ -1,6 +1,7 @@
 package kz.baltabayev.studentservice.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,7 +9,7 @@ import org.springframework.web.multipart.MultipartFile;
 @FeignClient(name = "storage-service", path = "/api/v1/image")
 public interface StorageServiceClient {
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     ResponseEntity<String> uploadImage(
             @RequestParam("source") String source,
             @RequestParam("target") Long target,
