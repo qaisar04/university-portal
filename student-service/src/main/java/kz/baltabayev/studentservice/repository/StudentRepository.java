8ff4af731c49922f53d1e5af7e3a147a9e6aa603
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-03-02 14:07:03 +0500

    fix

--- student-service/src/main/java/kz/baltabayev/studentservice/repository/StudentRepository.java
+++ student-service/src/main/java/kz/baltabayev/studentservice/repository/StudentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    List<Student> findAllByStudentInfo_FacultyId(Long facultyId);
+    List<Student> findAllByFacultyId(Long id);
 }
