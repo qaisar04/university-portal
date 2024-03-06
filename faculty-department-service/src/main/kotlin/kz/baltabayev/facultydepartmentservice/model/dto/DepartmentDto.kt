595236ba25f99396108fc8b8532b05b200eda09c
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-03-06 20:50:20 +0500

    validation

--- faculty-department-service/src/main/kotlin/kz/baltabayev/facultydepartmentservice/model/dto/DepartmentDto.kt
+++ faculty-department-service/src/main/kotlin/kz/baltabayev/facultydepartmentservice/model/dto/DepartmentDto.kt
@@ -1,6 +1,12 @@
 package kz.baltabayev.facultydepartmentservice.model.dto
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+
+
 data class DepartmentDto(
+    @NotNull @NotBlank
     var name: String? = null,
+    @NotNull @NotBlank
     var head: String? = null,
 )
--- faculty-department-service/src/main/kotlin/kz/baltabayev/facultydepartmentservice/model/dto/FacultyDto.kt
+++ faculty-department-service/src/main/kotlin/kz/baltabayev/facultydepartmentservice/model/dto/FacultyDto.kt
@@ -1,9 +1,13 @@
 package kz.baltabayev.facultydepartmentservice.model.dto
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 import kz.baltabayev.facultydepartmentservice.model.entity.Department
 
 data class FacultyDto(
+    @NotNull @NotBlank
     var name: String? = null,
+    @NotNull @NotBlank
     var dean: String? = null,
     var departments: List<Department> = listOf()
 )
