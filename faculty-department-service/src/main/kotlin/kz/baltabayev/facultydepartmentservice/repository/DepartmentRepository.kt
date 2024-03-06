25e94952bf7af6f2b4f055f8e19ea3dc3effa6a3
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-03-06 21:29:41 +0500

    annotations repository

--- faculty-department-service/src/main/kotlin/kz/baltabayev/facultydepartmentservice/repository/DepartmentRepository.kt
+++ faculty-department-service/src/main/kotlin/kz/baltabayev/facultydepartmentservice/repository/DepartmentRepository.kt
@@ -2,6 +2,8 @@ package kz.baltabayev.facultydepartmentservice.repository
 
 import kz.baltabayev.facultydepartmentservice.model.entity.Department
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface DepartmentRepository: JpaRepository<Department, Long> {
 }
--- faculty-department-service/src/main/kotlin/kz/baltabayev/facultydepartmentservice/repository/FacultyRepository.kt
+++ faculty-department-service/src/main/kotlin/kz/baltabayev/facultydepartmentservice/repository/FacultyRepository.kt
@@ -2,6 +2,8 @@ package kz.baltabayev.facultydepartmentservice.repository
 
 import kz.baltabayev.facultydepartmentservice.model.entity.Faculty
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface FacultyRepository: JpaRepository<Faculty, Long> {
 }
