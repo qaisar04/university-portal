9bc274bcc53f4ca9dfb246d6aeff0f9f344dc9c3
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-03-07 12:36:52 +0500

    resourse server kc init and changed endpoint

--- subject-service/src/main/java/kz/baltabayev/subjectservice/controller/SubjectController.java
+++ subject-service/src/main/java/kz/baltabayev/subjectservice/controller/SubjectController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/subject")
+@RequestMapping("api/v1/subject")
 @RequiredArgsConstructor
 public class SubjectController {
 
