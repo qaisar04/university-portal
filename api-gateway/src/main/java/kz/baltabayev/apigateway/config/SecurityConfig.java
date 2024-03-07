2c577971809c3c71e4dfb2cb120e76799a7ea7a3
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-03-07 23:34:19 +0500

    kc

--- api-gateway/src/main/java/kz/baltabayev/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/kz/baltabayev/apigateway/config/SecurityConfig.java
@@ -1,53 +1,27 @@
-//package kz.baltabayev.apigateway.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-//import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
-//import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-//import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
-//import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//import java.text.SimpleDateFormat;
-//
-//@Configuration
-//@EnableWebFluxSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(request ->
-//                        request.requestMatchers("/eureka/**").permitAll()
-//                                .anyRequest().authenticated())
-//                .sessionManagement(httpSecuritySessionManagementConfigurer ->
-//                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .oauth2ResourceServer(configurer -> configurer.jwt(Customizer.withDefaults()))
-//                .build();
-//    }
-//
-//    @Bean
-//    public OAuth2UserService<OidcUserRequest, OidcUser> oAuth2UserService() {
-//        OidcUserService userService = new OidcUserService();
-//        return userRequest -> {
-//            OidcUser oidcUser = userService.loadUser(userRequest);
-//            var authorities = oidcUser.getClaimAsStringList("spring_sec_roles").stream()
-//                    .map(SimpleDateFormat::new)
-//                    .map(GrantedAuthority.class::cast)
-//                    .toList();
-//
-//            return new DefaultOidcUser(authorities,
-//                    oidcUser.getIdToken(), oidcUser.getUserInfo());
-//        };
-//    }
-//}
+package kz.baltabayev.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .securityMatcher(new PathPatternParserServerWebExchangeMatcher("/actuator/**"))
+                .authorizeExchange(configurer -> configurer
+                        .pathMatchers("/actuator/health", "/actuator/health/**", "/eureka/*").permitAll())
+                .oauth2ResourceServer(configurer -> configurer.jwt(Customizer.withDefaults()))
+                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .build();
+    }
+}
