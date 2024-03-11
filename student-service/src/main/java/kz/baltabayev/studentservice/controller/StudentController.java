fe886440b4aea1f6eee35c944e41e81d19d0d933
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-03-11 21:23:08 +0500

    kc resource server

--- student-service/src/main/java/kz/baltabayev/studentservice/controller/StudentController.java
+++ student-service/src/main/java/kz/baltabayev/studentservice/controller/StudentController.java
@@ -8,6 +8,7 @@ import kz.baltabayev.studentservice.model.entity.Student;
 import kz.baltabayev.studentservice.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
