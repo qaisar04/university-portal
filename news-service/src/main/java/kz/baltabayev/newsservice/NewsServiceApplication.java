dd685a839924ef91fdf3e17286b64761598455d4
Qaisar Baltabayev <q.baltabayev@icloud.com>
2024-03-13 11:33:48 +0500

    init news service

---
+++ news-service/src/main/java/kz/baltabayev/newsservice/NewsServiceApplication.java
@@ -0,0 +1,12 @@
+package kz.baltabayev.newsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsServiceApplication.class, args);
+    }
+}
